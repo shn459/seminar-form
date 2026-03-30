0f8a00bdaa3c284fad21573f8adc6cdc9786eb3d
shn459 <n.shn459@gmail.com>
2026-03-31 00:04:06 +0900

    Update proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -5,6 +5,7 @@ function postWithRedirect(url, body, redirectCount) {
   if (redirectCount > 5) return Promise.reject(new Error('Too many redirects'));
 
   return new Promise((resolve, reject) => {
+    console.log('Requesting URL:', url, 'redirect count:', redirectCount);
     const req = https.request(url, {
       method: 'POST',
       headers: {
@@ -13,41 +14,12 @@ function postWithRedirect(url, body, redirectCount) {
         'User-Agent': 'Mozilla/5.0'
       }
     }, (res) => {
+      console.log('Status:', res.statusCode, 'Location:', res.headers.location);
       if (res.statusCode >= 300 && res.statusCode < 400 && res.headers.location) {
         res.resume();
         resolve(postWithRedirect(res.headers.location, body, redirectCount + 1));
       } else {
         let data = '';
         res.on('data', chunk => { data += chunk; });
-        res.on('end', () => resolve(data));
-      }
-    });
-
-    req.on('error', reject);
-    req.write(body);
-    req.end();
-  });
-}
-
-exports.handler = async function(event) {
-  const GAS_URL = 'https://script.google.com/macros/s/AKfycbzl9Gq_uLvQ02l56QxyHCm0R3-8X3NVOA3qfEGsEwQ72c8qnUniIIChgN-cMc8l6Wda/exec';
-  const body = event.body || '{}';
-
-  try {
-    const data = await postWithRedirect(GAS_URL, body);
-    return {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: data
-    };
-  } catch(e) {
-    return {
-      statusCode: 500,
-      headers: { 'Access-Control-Allow-Origin': '*' },
-      body: JSON.stringify({ error: e.message })
-    };
-  }
-};
+        res.on('end', () => {
+          console.log('Response data (​​​​​​​​​​​​​​​​
