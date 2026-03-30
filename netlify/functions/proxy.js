dce647a121a4fcfe005f718715bd56f86085f269
shn459 <n.shn459@gmail.com>
2026-03-30 23:50:03 +0900

    Update proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,15 +1,24 @@
 const https = require('https');
+const http = require('http');
 
 function fetchWithRedirect(url, redirectCount) {
   redirectCount = redirectCount || 0;
-  if (redirectCount > 5) {
+  if (redirectCount > 10) {
     return Promise.reject(new Error('Too many redirects'));
   }
 
   return new Promise((resolve, reject) => {
-    https.get(url, { headers: { 'User-Agent': 'Mozilla/5.0' } }, (res) => {
+    const lib = url.startsWith('https') ? https : http;
+    lib.get(url, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0',
+        'Accept': 'application/json, text/javascript, */*'
+      }
+    }, (res) => {
       if (res.statusCode >= 300 && res.statusCode < 400 && res.headers.location) {
-        resolve(fetchWithRedirect(res.headers.location, redirectCount + 1));
+        const location = res.headers.location;
+        res.resume();
+        resolve(fetchWithRedirect(location, redirectCount + 1));
       } else {
         let data = '';
         res.on('data', chunk => { data += chunk; });
@@ -21,7 +30,11 @@ function fetchWithRedirect(url, redirectCount) {
 
 exports.handler = async function(event) {
   const params = event.queryStringParameters || {};
-  const query = Object.keys(params).map(k => k + '=' + encodeURIComponent(params[k])).join('&');
+  const query = Object.keys(params)
+    .filter(k => k !== 'callback')
+    .map(k => k + '=' + encodeURIComponent(params[k]))
+    .join('&');
+  
   const gasUrl = 'https://script.google.com/macros/s/AKfycbwOSbW0ThYTMNlxI-Y13v3lIFjtGEs9wvHxlxQUO9IRWdw-cwz3sDnALiuMqIqaxo5n/exec?' + query;
 
   try {
