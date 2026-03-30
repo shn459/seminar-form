d01ea0a6f705e46be826018cc9794ec88f501ed6
shn459 <n.shn459@gmail.com>
2026-03-30 23:45:41 +0900

    Update proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,26 +1,44 @@
 const https = require('https');
 
+function fetchWithRedirect(url, redirectCount) {
+  redirectCount = redirectCount || 0;
+  if (redirectCount > 5) {
+    return Promise.reject(new Error('Too many redirects'));
+  }
+
+  return new Promise((resolve, reject) => {
+    https.get(url, { headers: { 'User-Agent': 'Mozilla/5.0' } }, (res) => {
+      if (res.statusCode >= 300 && res.statusCode < 400 && res.headers.location) {
+        resolve(fetchWithRedirect(res.headers.location, redirectCount + 1));
+      } else {
+        let data = '';
+        res.on('data', chunk => { data += chunk; });
+        res.on('end', () => resolve(data));
+      }
+    }).on('error', reject);
+  });
+}
+
 exports.handler = async function(event) {
   const params = event.queryStringParameters || {};
   const query = Object.keys(params).map(k => k + '=' + encodeURIComponent(params[k])).join('&');
   const gasUrl = 'https://script.google.com/macros/s/AKfycbwOSbW0ThYTMNlxI-Y13v3lIFjtGEs9wvHxlxQUO9IRWdw-cwz3sDnALiuMqIqaxo5n/exec?' + query;
 
-  return new Promise((resolve) => {
-    https.get(gasUrl, { headers: { 'User-Agent': 'Mozilla/5.0' } }, (res) => {
-      let data = '';
-      res.on('data', chunk => { data += chunk; });
-      res.on('end', () => {
-        resolve({
-          statusCode: 200,
-          headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-          },
-          body: data
-        });
-      });
-    }).on('error', (e) => {
-      resolve({ statusCode: 500, body: JSON.stringify({ error: e.message }) });
-    });
-  });
+  try {
+    const data = await fetchWithRedirect(gasUrl);
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: data
+    };
+  } catch(e) {
+    return {
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: e.message })
+    };
+  }
 };
