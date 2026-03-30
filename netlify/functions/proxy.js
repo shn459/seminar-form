ae1374c87627ae1d560678187530e82a4af6fb01
shn459 <n.shn459@gmail.com>
2026-03-31 00:40:59 +0900

    Update proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,43 +1,122 @@
 const https = require('https');
+const crypto = require('crypto');
 
-function getWithRedirect(url, redirectCount) {
-  redirectCount = redirectCount || 0;
-  if (redirectCount > 10) return Promise.reject(new Error('Too many redirects'));
+function base64url(str) {
+  return Buffer.from(str).toString('base64')
+    .replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+}
+
+function getAccessToken(clientEmail, privateKey) {
+  var now = Math.floor(Date.now() / 1000);
+  var header = base64url(JSON.stringify({ alg: 'RS256', typ: 'JWT' }));
+  var claim = base64url(JSON.stringify({
+    iss: clientEmail,
+    scope: 'https://www.googleapis.com/auth/script.projects https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/calendar',
+    aud: 'https://oauth2.googleapis.com/token',
+    exp: now + 3600,
+    iat: now
+  }));
+
+  var sign = crypto.createSign('RSA-SHA256');
+  sign.update(header + '.' + claim);
+  var signature = sign.sign(privateKey, 'base64')
+    .replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+
+  var jwt = header + '.' + claim + '.' + signature;
 
   return new Promise(function(resolve, reject) {
-    https.get(url, {
-      headers: { 'User-Agent': 'Mozilla/5.0' }
-    }, function(res) {
-      if (res.statusCode >= 300 && res.statusCode < 400 && res.headers.location) {
-        res.resume();
-        resolve(getWithRedirect(res.headers.location, redirectCount + 1));
-      } else {
-        var data = '';
-        res.on('data', function(chunk) { data += chunk; });
-        res.on('end', function() { resolve(data); });
+    var body = 'grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Ajwt-bearer&assertion=' + jwt;
+    var req = https.request({
+      hostname: 'oauth2.googleapis.com',
+      path: '/token',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(body)
       }
-    }).on('error', reject);
+    }, function(res) {
+      var data = '';
+      res.on('data', function(chunk) { data += chunk; });
+      res.on('end', function() {
+        var parsed = JSON.parse(data);
+        if (parsed.access_token) resolve(parsed.access_token);
+        else reject(new Error('Token error: ' + data));
+      });
+    });
+    req.on('error', reject);
+    req.write(body);
+    req.end();
+  });
+}
+
+function callGAS(scriptId, functionName, parameters, accessToken) {
+  var body = JSON.stringify({
+    function: functionName,
+    parameters: parameters
+  });
+
+  return new Promise(function(resolve, reject) {
+    var req = https.request({
+      hostname: 'script.googleapis.com',
+      path: '/v1/scripts/' + scriptId + ':run',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + accessToken,
+        'Content-Length': Buffer.byteLength(body)
+      }
+    }, function(res) {
+      var data = '';
+      res.on('data', function(chunk) { data += chunk; });
+      res.on('end', function() { resolve(data); });
+    });
+    req.on('error', reject);
+    req.write(body);
+    req.end();
   });
 }
 
 exports.handler = function(event, context, callback) {
-  var GAS_URL = 'https://script.google.com/macros/s/AKfycbxMip0eOAxtY-RHMilvcDzkAcVrU8iNnRMG4u7HKSh2vWyv2U9MHN8Vxn74gFJX8M7o/exec';
-  
-  var params = event.queryStringParameters || {};
-  var query = Object.keys(params).map(function(k) {
-    return k + '=' + encodeURIComponent(params[k]);
-  }).join('&');
+  var SCRIPT_ID = '1hvwBkEWQiHePk-1tcG0be4SCSn_RHfRIRzUOf1_rFGyfww6NwKLsL3RQ';
+  var clientEmail = process.env.GAS_CLIENT_EMAIL;
+  var privateKey = (process.env.GAS_PRIVATE_KEY || '').replace(/\\n/g, '\n');
 
-  var url = GAS_URL + (query ? '?' + query : '');
+  var params = {};
+  try { params = JSON.parse(event.body || '{}'); } catch(e) {}
 
-  getWithRedirect(url).then(function(data) {
+  var action = params.action;
+  var functionName;
+  var parameters = [];
+
+  if (action === 'getSettings') {
+    functionName = 'getSettings';
+  } else if (action === 'getSlots') {
+    functionName = 'getAvailableSlots';
+  } else if (action === 'bookSlot') {
+    functionName = 'bookSlot';
+    parameters = [params.eventId, params.formData];
+  } else if (action === 'saveToSheet') {
+    functionName = 'saveToSheet';
+    parameters = [params.formData];
+  } else {
+    callback(null, { statusCode: 200, body: '"ready"' });
+    return;
+  }
+
+  getAccessToken(clientEmail, privateKey).then(function(token) {
+    return callGAS(SCRIPT_ID, functionName, parameters, token);
+  }).then(function(data) {
+    var parsed = JSON.parse(data);
+    var result = parsed.response && parsed.response.result !== undefined
+      ? parsed.response.result
+      : parsed;
     callback(null, {
       statusCode: 200,
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*'
       },
-      body: data
+      body: JSON.stringify(result)
     });
   }).catch(function(e) {
     callback(null, {
